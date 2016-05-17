db4dd4390d35ffb471f2eff8ae9217f3ccc54f1e
JanelleHeron <jheron3@my.centennialcollege.ca>
2016-05-17 12:57:50 -0400

    Version: 0.0.2 - Added a reference to the driver class from the UnitTest

--- COMP123_S2016-L3/Program.cs
+++ COMP123_S2016-L3/Program.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+ *  Author: Janelle Heron
+ *  Date Created: May 17 2016
+ *  Description: Advanced Methods Demo for Lesson 2/3
+ *  Version: 0.0.2 - Added a reference to the 
+ *                   driver class from the UnitTest
+ *  
+ */
+
+
+
 namespace COMP123_S2016_L3
 {
+
+ /**
+ * Main driver class
+ * 
+ * @class Program
+ */
+
     class Program
     {
+        /**
+         *  Main method for our driver class
+         *  
+         *  @constructor Main
+         *  @param {string[]} args
+         */
         static void Main(string[] args)
         {
+
         }
     }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_S2016_L3;
 
+
+/**
+ *  Author: Janelle Heron
+ *  Date Created: May 17 2016
+ *  Description: Advanced 
+ */
 namespace UnitTestProjectForLesson3
 {
     [TestClass]
@@ -9,6 +16,11 @@ namespace UnitTestProjectForLesson3
         [TestMethod]
         public void TestMethod1()
         {
+            //Range
+
+            //Act
+
+            //Assert
         }
     }
 }
