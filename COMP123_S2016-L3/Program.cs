6914f4c59a93e992654b95a32d3a826cb3224ce1
JanelleHeron <jheron3@my.centennialcollege.ca>
2016-05-17 13:44:59 -0400

    Version: 0.0.4 - Added addYandX method and associated test

--- COMP123_S2016-L3/Program.cs
+++ COMP123_S2016-L3/Program.cs
@@ -7,9 +7,8 @@ using System.Threading.Tasks;
 /**
  *  Author: Janelle Heron
  *  Date Created: May 17 2016
- *  Description: Advanced Methods Demo for Lesson 2/3
- *  Version: 0.0.2 - Added a reference to the 
- *                   driver class from the UnitTest
+ *  Description: Advanced Methods Demo for Lesson 2
+ *  Version: 0.0.4 - Added addYandX method and associated test
  *  
  */
 
@@ -22,7 +21,7 @@ namespace COMP123_S2016_L3
  * @class Program
  */
 
-    class Program
+    public class Program
     {
         /**
          *  Main method for our driver class
@@ -32,7 +31,47 @@ namespace COMP123_S2016_L3
          */
         static void Main(string[] args)
         {
+            int x = 50;
 
+            int y = 40;
+
+            int result = 0;
+
+            result = addXandY(x, y);
+            Console.WriteLine(result);
+            Console.WriteLine();
+            addYtoX(ref x, ref y);
+            Console.WriteLine(x);
+        }
+        
+        /**
+         *  Simple method to add two values
+         *  
+         *  @methid addXandY
+         *  @param {int} firstNum
+         *  @param {int} secondNum
+         */
+
+        public static int addXandY(int firstNum, int secondNum)
+        {
+            int result = 0;
+            result = firstNum + secondNum;
+            return result;
+        }
+
+        /**
+         * Simple method to demonstrate pass by reference with ref keyword
+         * 
+         * @method addYtoX
+         * @param {ref int} X
+         * @param {ref int} Y
+         */
+
+        public static int addYtoX(ref int X, ref int Y)
+        {
+            X += Y;
+
+            return X;
         }
     }
 }
---
+++ UnitTestProject1/UnitTestForLesson2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_S2016_L3;
+
+
+/**
+ *  Author: Janelle Heron
+ *  Date Created: May 17 2016
+ *  Description: Advanced 
+ */
+namespace UnitTestProjectForLesson2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddXandYTestMethod()
+        {
+            //Arrange
+
+            int x = 40;
+            int y = 50;
+            int expectedResult = 90;
+
+            //Act
+
+            int result = Program.addXandY(x, y);
+
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+
+        }
+
+
+      
+            [TestMethod]
+            public void AddYtoXTestMethod()
+            {
+                //Arrange
+
+                int x = 40;
+                int y = 50;
+                int expectedResult = 90;
+
+                //Act
+
+                int result = Program.addYtoX(ref x, ref y);
+
+
+                //Assert
+                Assert.AreEqual(expectedResult, result);
+
+            }
+        }
+}
